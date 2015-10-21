0ef229195c5565c4eb1b09b8921ad4458628a4aa
Niko <kod.nik@gmx.at>
2015-10-22 01:08:20 +0200

    Rechnung erstellt

---
+++ src/domain/Rechnung.java
@@ -0,0 +1,57 @@
+package domain;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+public class Rechnung implements Domain{
+
+    /**
+     * Eindeutige ID einer Rechnung
+     */
+    long id;
+
+    /**
+     * Datum der Rechnung, fungiert als lesbare Bezeichnung
+     */
+    Timestamp date;
+
+    /**
+     * Liste der Artikel auf der Rechnung
+     */
+    ArrayList<Artikel> artikelListe;
+
+    /**
+     * Konstruktor für die Rechnung
+     * @param id            Eindeutige ID der Rechnung
+     * @param date          Zeitpunkt, an dem die Rechnung erstellt wurde
+     * @param artikelListe  Liste der Artikel auf der Rechnung
+     */
+    public Rechnung(long id, Timestamp date, ArrayList<Artikel> artikelListe){
+        this.id = id;
+        this.date = date;
+        this.artikelListe = artikelListe;
+    }
+
+    /**
+     * Konstruktor für die Rechnung mit lehrer Artikelliste
+     * @param id    Eindeutige ID der Rechnung
+     * @param date  Zetpunkt, an dem die Rechnung erstellt wurde
+     */
+    public Rechnung(long id, Timestamp date){
+        this.id = id;
+        this.date = date;
+        artikelListe = new ArrayList<Artikel>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public ArrayList<Artikel> getArtikelListe() {
+        return artikelListe;
+    }
+}
